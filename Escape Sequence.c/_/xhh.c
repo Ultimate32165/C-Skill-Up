3b9f736469fde7900ad17a3139262126a07bd242
Ultimate32165 <hasanmaruf2019@gmail.com>
2025-09-01 21:20:51 +0600

    f

---
+++ Escape Sequence.c/_/xhh.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // We are using \xhh escape sequence.
+    // Here hh is one or more hexadecimal
+    // digits(0....9, a...f, A...F).
+    char* s = "B\x4a";
+    printf("%s", s);
+    return 0;
+}
